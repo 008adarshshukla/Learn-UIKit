095b3418fdb8439da8bfc2d7016890c51fcd1a61
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-17 16:46:05 +0530

    Done scroll View

--- LearnUIKit/ScrollViewController.swift
+++ LearnUIKit/ScrollViewController.swift
@@ -11,12 +11,36 @@ class ScrollViewController: UIViewController {
     
     var programmaticScrollView: UIScrollView!
     var height : CGFloat = 0.0
+    var myLabel: UILabel!
+    var gotoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //label programmatically.
+        myLabel = UILabel()
+        myLabel.frame = CGRect(x: 0, y: 60, width: self.view.frame.width, height: 40)
+        myLabel.text = "Scroll Status..."
+        myLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        myLabel.textAlignment = .center
+        
+        self.view.addSubview(myLabel)
+        
+        //creating buttom programmatically.
+        gotoButton = UIButton()
+        gotoButton.frame = CGRect(x: 10, y: 120, width: 300, height: 45)
+        gotoButton.setTitle("Go to", for: .normal)
+        gotoButton.backgroundColor = UIColor.blue
+        gotoButton.center.x = self.view.center.x
+        gotoButton.layer.cornerRadius = 10
+        gotoButton.addTarget(self, action: #selector(goToClicked), for: .touchUpInside)
+        
+        self.view.addSubview(gotoButton)
+        
+        //scrollview programmatically.
         programmaticScrollView = UIScrollView()
-        programmaticScrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        programmaticScrollView.delegate = self
+        programmaticScrollView.frame = CGRect(x: 0, y: 185, width: self.view.frame.width, height: self.view.frame.height)
         
         //content offset - indicates the current offset of the scroll view's content view from its original position.
         programmaticScrollView.contentOffset = CGPoint(x: 0, y: 0)
@@ -60,9 +84,82 @@ class ScrollViewController: UIViewController {
             height = rectangleView.frame.maxY
         }
         
-        programmaticScrollView.contentSize = CGSize(width: self.view.frame.width, height: self.height + 10)
+        programmaticScrollView.contentSize = CGSize(width: self.view.frame.width, height: self.height + 200)
         
         self.view.addSubview(programmaticScrollView)
         
     }
+    
+    
+    @objc func goToClicked() {
+        
+    }
+}
+
+//extending th ScrollViewController to implement delgate maethods.
+extension ScrollViewController: UIScrollViewDelegate {
+    
+    //This method is called when the scroll view's content offset changes. You can use this method to respond to     changes in the scroll view's content offset, such as updating the position of other views or updating the content of the scroll view.
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("Did scroll")
+    }
+    
+    //This method is called when the user starts dragging the scroll view. You can use this method to perform any setup or updates that need to be done before the scroll view starts scrolling.
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        print("Will begin dragging")
+    }
+    
+    // This method is called when the user stops dragging the scroll view. You can use this method to perform any cleanup or updates that need to be done after the scroll view stops scrolling.
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        print("Will end dragging")
+    }
+    
+    // secondly exec when dragging ends
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        print("Did end dragging")
+    }
+    
+    // This method is called when the scroll view is about to start decelerating after the user stops dragging it. You can use this method to perform any setup or updates that need to be done before the scroll view starts decelerating.
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        print("Will begin decel")
+    }
+    
+    // This method is called when the scroll view has finished decelerating after the user stops dragging it.
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("Did end decel")
+    }
+    
+    // Called when the scroll view finishes scrolling programmatically.
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        print("SetContentOffset")
+    }
+    
+    // Called when the user begins to zoom the content view.
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        
+    }
+    
+    
+    // Called when the content view finishes zooming.
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
+    }
+    
+    // Called when the content view zooms in or out.
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        print("Did zoom")
+    }
+    
+    // Called when the content view scrolls to the top, after the user taps the status bar.
+    func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
+        print("ScrollToTop")
+    }
+    
+    // Called when the user taps the status bar, allowing for custom behavior when the content view scrolls to the top.
+    func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+        print("Should ScrollToTop")
+        return true
+    }
+    
+    
 }
